e01209c9cf35bdb27f468e44aa4af00a34c43c37
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-04-21 20:45:34 +0100

    Deeper Policy Net (#73)
    Elo   | 13.87 +- 8.50 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.99 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 4460 W: 1639 L: 1461 D: 1360
    Penta | [164, 433, 909, 509, 215]
    https://chess.swehosting.se/test/6251/
    
    Bench: 106629

--- src/games/chess/policy.rs
+++ src/games/chess/policy.rs
@@ -6,12 +6,14 @@ use goober::{activation, layer, FeedForwardNetwork, Matrix, SparseVector, Vector
 #[derive(Clone, Copy, FeedForwardNetwork)]
 pub struct SubNet {
     ft: layer::SparseConnected<activation::ReLU, 768, 16>,
+    l2: layer::DenseConnected<activation::ReLU, 16, 16>,
 }
 
 impl SubNet {
     pub const fn zeroed() -> Self {
         Self {
             ft: layer::SparseConnected::zeroed(),
+            l2: layer::DenseConnected::zeroed(),
         }
     }
 
@@ -19,8 +21,12 @@ impl SubNet {
         let matrix = Matrix::from_fn(|_, _| f());
         let vector = Vector::from_fn(|_| f());
 
+        let matrix2 = Matrix::from_fn(|_, _| f());
+        let vector2 = Vector::from_fn(|_| f());
+
         Self {
             ft: layer::SparseConnected::from_raw(matrix, vector),
+            l2: layer::DenseConnected::from_raw(matrix2, vector2),
         }
     }
 }
