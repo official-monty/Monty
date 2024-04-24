4f909fd6f17dece24ab1994d74848f5237ec3ed4
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-04-24 16:56:15 +0100

    New Value Net (#75)
    Elo   | 13.63 +- 8.37 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.91 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 4590 W: 1682 L: 1502 D: 1406
    Penta | [199, 420, 908, 538, 230]
    https://chess.swehosting.se/test/6315/
    
    Bench: 101727

--- train/value/src/bin/chess.rs
+++ train/value/src/bin/chess.rs
@@ -34,7 +34,7 @@ fn main() {
         .build();
 
     let schedule = TrainingSchedule {
-        net_id: "chess-value5k".to_string(),
+        net_id: "chess-value5".to_string(),
         eval_scale: 400.0,
         ft_regularisation: 0.0,
         batch_size: 16_384,
@@ -53,7 +53,7 @@ fn main() {
 
     let settings = LocalSettings {
         threads: 4,
-        data_file_paths: vec!["data/chess/value007-4.data"],
+        data_file_paths: vec!["data/chess/value5.data"],
         output_directory: "checkpoints",
     };
 
