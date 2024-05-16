a94b742321a38bfde90902bac678d0dae602386d
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-05-16 16:42:05 +0100

    Another Value Network (#88)
    Elo   | 27.32 +- 11.66 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=64MB
    LLR   | 2.93 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 2294 W: 974 L: 794 D: 526
    Penta | [103, 193, 452, 219, 180]
    https://chess.swehosting.se/test/6667/
    
    Bench: 128195

--- train/value/src/bin/chess.rs
+++ train/value/src/bin/chess.rs
@@ -34,7 +34,7 @@ fn main() {
         .build();
 
     let schedule = TrainingSchedule {
-        net_id: "new-data".to_string(),
+        net_id: "moar-data".to_string(),
         eval_scale: 400.0,
         ft_regularisation: 0.0,
         batch_size: 16_384,
