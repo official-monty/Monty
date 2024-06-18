a59c6a56e349156bf4218c4dcf3c9bad2453a273
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-06-18 03:00:19 +0100

    Increase bench depth (#9)
    From 5 to 7. Typical result:
    
    1592579 nodes 402281 nps
    
    Reduces bench collisions and nps variability
    
    Bench: 1592579

--- src/chess.rs
+++ src/chess.rs
@@ -52,7 +52,7 @@ impl Default for ChessState {
 
 impl ChessState {
     pub const STARTPOS: &'static str = STARTPOS;
-    pub const BENCH_DEPTH: usize = 5;
+    pub const BENCH_DEPTH: usize = 7;
 
     pub fn bbs(&self) -> [u64; 8] {
         self.board.bbs()
