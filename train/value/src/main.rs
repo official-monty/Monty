0c3729750fd5a56551ee6aa6f55410edb195c99f
jw1912 <jamie.whiting.dev@gmail.com>
2024-07-17 10:38:03 +0100

    Bench: 2292514

--- train/value/src/main.rs
+++ train/value/src/main.rs
@@ -36,7 +36,7 @@ fn main() {
         .build();
 
     let schedule = TrainingSchedule {
-        net_id: "datagen0-1-2-3-dfrc25m".to_string(),
+        net_id: "datagen0-4".to_string(),
         eval_scale: 400.0,
         ft_regularisation: 0.0,
         batch_size: 16_384,
@@ -55,7 +55,7 @@ fn main() {
 
     let settings = LocalSettings {
         threads: 4,
-        data_file_paths: vec!["../monty-data/datagen0-1-2-3-dfrc25m.data"],
+        data_file_paths: vec!["../monty-data/datagen0-4.data"],
         output_directory: "checkpoints",
     };
 
