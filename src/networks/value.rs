3ab62793784ac48db792c28c9003e43548d30b00
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-07-18 13:20:01 +0100

    New Value Network: `nn-d75a5bf64093.network` (#23)
    Data Used:
    - 6695b5891dbf900f9c8b59fc
    - 669449111dbf900f9c8b58e6
    - 66933f591dbf900f9c8b57c3
    - 669075871dbf900f9c8b5454
    - 669167f81dbf900f9c8b55e3
    - 669017281dbf900f9c8b53ad
    - 669016bf1dbf900f9c8b53aa
    
    Passed STC:
    LLR: 3.16 (-2.94,2.94) <0.00,4.00>
    Total: 5216 W: 1474 L: 1284 D: 2458
    Ptnml(0-2): 81, 563, 1163, 687, 114
    https://montychess.org/tests/view/6697922041a4a35b6ff04cdc
    
    Passed LTC:
    LLR: 2.94 (-2.94,2.94) <1.00,5.00>
    Total: 12750 W: 3167 L: 2954 D: 6629
    Ptnml(0-2): 142, 1471, 2953, 1650, 159
    https://montychess.org/tests/view/6697b91941a4a35b6ff04d19
    
    Bench: 2292514

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -2,7 +2,7 @@ use crate::Board;
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const ValueFileDefaultName: &str = "nn-6ee78ecb5a56.network";
+pub const ValueFileDefaultName: &str = "nn-d75a5bf64093.network";
 
 const SCALE: i32 = 400;
 
