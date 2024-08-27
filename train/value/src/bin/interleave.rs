c9a8ea1d68a6db8a194d03a1679d667e9c5c4b68
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-08-27 17:39:07 +0100

    Load Direct from `binpack` in Value Training (#51)
    Facilitates much larger training runs.
    
    Bench: 1316421

---
+++ train/value/src/bin/interleave.rs
@@ -0,0 +1,98 @@
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter, Write},
+};
+
+use datagen::Binpack;
+
+fn main() -> std::io::Result<()> {
+    let inputs = [
+        "../binpacks/bestmove-q.binpack",
+        "../binpacks/root-q.binpack",
+    ];
+    let output = "interleaved-value.binpack";
+
+    println!("Writing to {:#?}", output);
+    println!("Reading from:\n{:#?}", inputs);
+    let mut streams = Vec::new();
+    let mut total = 0;
+
+    let target = File::create(output)?;
+    let mut writer = BufWriter::new(target);
+
+    for path in &inputs {
+        let file = File::open(path)?;
+
+        let count = file.metadata()?.len();
+
+        if count > 0 {
+            streams.push((count, BufReader::new(file)));
+            total += count;
+        }
+    }
+
+    let mut remaining = total;
+    let mut rng = RandU64::default();
+
+    const INTERVAL: u64 = 1024 * 1024 * 256;
+    let mut prev = remaining / INTERVAL;
+
+    let mut buffer = Vec::new();
+
+    while remaining > 0 {
+        let mut spot = rng.rand() % remaining;
+        let mut idx = 0;
+        while streams[idx].0 < spot {
+            spot -= streams[idx].0;
+            idx += 1;
+        }
+
+        let (count, reader) = &mut streams[idx];
+
+        Binpack::deserialise_fast_into_buffer(reader, &mut buffer)?;
+        writer.write_all(&buffer)?;
+
+        let size = buffer.len() as u64;
+
+        remaining -= size;
+        *count -= size;
+        if *count == 0 {
+            streams.swap_remove(idx);
+        }
+
+        if remaining / INTERVAL < prev {
+            prev = remaining / INTERVAL;
+            let written = total - remaining;
+            print!(
+                "Written {written}/{total} Bytes ({:.2}%)\r",
+                written as f64 / total as f64 * 100.0
+            );
+            let _ = std::io::stdout().flush();
+        }
+    }
+
+    Ok(())
+}
+
+struct RandU64(u64);
+
+impl Default for RandU64 {
+    fn default() -> Self {
+        Self(
+            (std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("valid")
+                .as_nanos()
+                & 0xFFFF_FFFF_FFFF_FFFF) as u64,
+        )
+    }
+}
+
+impl RandU64 {
+    fn rand(&mut self) -> u64 {
+        self.0 ^= self.0 << 13;
+        self.0 ^= self.0 >> 7;
+        self.0 ^= self.0 << 17;
+        self.0
+    }
+}
