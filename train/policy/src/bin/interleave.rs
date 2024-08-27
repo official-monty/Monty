93add145a5cec785e2561d123c792ae358ba3c6b
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2024-08-27 15:45:26 +0100

    Load Direct from `binpack` in Policy Training (#50)
    Facilitates much larger training runs.
    
    Bench: 1316421

--- train/policy/src/bin/interleave.rs
+++ train/policy/src/bin/interleave.rs
@@ -1,33 +1,30 @@
 use std::{
     fs::File,
-    io::{BufReader, BufWriter, Read, Write},
+    io::{BufReader, BufWriter, Write},
 };
 
-use datagen::Rand;
+use montyformat::MontyFormat;
 
-const SIZE: usize = 512;
+fn main() -> std::io::Result<()> {
+    let inputs = [
+        "../binpacks/policygen4.binpack",
+        "../binpacks/policygen4-dfrc.binpack",
+        "../binpacks/policygen6.binpack",
+    ];
+    let output = "interleaved.binpack";
 
-fn main() {
-    let mut args = std::env::args();
-    args.next();
-
-    let collected: Vec<String> = args.collect();
-
-    assert!(collected.len() > 1);
-
-    let output = collected.last().unwrap();
-    println!("Writing to {output}");
-    let inputs: Vec<_> = collected.iter().take(collected.len() - 1).collect();
-    println!("Reading from:\n{inputs:#?}");
+    println!("Writing to {:#?}", output);
+    println!("Reading from:\n{:#?}", inputs);
     let mut streams = Vec::new();
     let mut total = 0;
 
-    let target = File::create(output.as_str()).unwrap();
+    let target = File::create(output)?;
     let mut writer = BufWriter::new(target);
 
-    for path in inputs {
-        let file = File::open(path.as_str()).unwrap();
-        let count = file.metadata().unwrap().len() as usize / SIZE;
+    for path in &inputs {
+        let file = File::open(path)?;
+
+        let count = file.metadata()?.len();
 
         if count > 0 {
             streams.push((count, BufReader::new(file)));
@@ -36,10 +33,15 @@ fn main() {
     }
 
     let mut remaining = total;
-    let mut rng = Rand::with_seed();
+    let mut rng = RandU64::default();
+
+    const INTERVAL: u64 = 1024 * 1024 * 256;
+    let mut prev = remaining / INTERVAL;
+
+    let mut buffer = Vec::new();
 
     while remaining > 0 {
-        let mut spot = rng.rand_int() as usize % remaining;
+        let mut spot = rng.rand() % remaining;
         let mut idx = 0;
         while streams[idx].0 < spot {
             spot -= streams[idx].0;
@@ -47,23 +49,51 @@ fn main() {
         }
 
         let (count, reader) = &mut streams[idx];
-        let mut value = [0; SIZE];
-        reader.read_exact(&mut value).unwrap();
-        writer.write_all(&value).unwrap();
 
-        remaining -= 1;
-        *count -= 1;
+        MontyFormat::deserialise_fast_into_buffer(reader, &mut buffer)?;
+        writer.write_all(&buffer)?;
+
+        let size = buffer.len() as u64;
+
+        remaining -= size;
+        *count -= size;
         if *count == 0 {
             streams.swap_remove(idx);
         }
 
-        if remaining % 16384 == 0 {
+        if remaining / INTERVAL < prev {
+            prev = remaining / INTERVAL;
             let written = total - remaining;
             print!(
-                "Written {written} / {total} ({:.2})\r",
-                written as f32 / total as f32 * 100.0
+                "Written {written}/{total} Bytes ({:.2}%)\r",
+                written as f64 / total as f64 * 100.0
             );
             let _ = std::io::stdout().flush();
         }
+    }
+
+    Ok(())
+}
+
+struct RandU64(u64);
+
+impl Default for RandU64 {
+    fn default() -> Self {
+        Self(
+            (std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("valid")
+                .as_nanos()
+                & 0xFFFF_FFFF_FFFF_FFFF) as u64,
+        )
+    }
+}
+
+impl RandU64 {
+    fn rand(&mut self) -> u64 {
+        self.0 ^= self.0 << 13;
+        self.0 ^= self.0 >> 7;
+        self.0 ^= self.0 << 17;
+        self.0
     }
 }
