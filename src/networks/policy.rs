98fe01179a6dc5c216e11d7cd3a4ca3f6527d238
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-09-02 01:05:51 +0100

    Policy L2 32
    bench 1116723

--- src/networks/policy.rs
+++ src/networks/policy.rs
@@ -9,17 +9,17 @@ const QA: i16 = 512;
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const PolicyFileDefaultName: &str = "nn-e2a03baa505c.network";
+pub const PolicyFileDefaultName: &str = "nn-1b01b6e89ea1.network";
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct SubNet {
-    ft: Layer<i16, 768, 16>,
-    l2: Layer<f32, 16, 16>,
+    ft: Layer<i16, 768, 32>,
+    l2: Layer<f32, 32, 32>,
 }
 
 impl SubNet {
-    fn out(&self, feats: &[usize]) -> Accumulator<f32, 16> {
+    fn out(&self, feats: &[usize]) -> Accumulator<f32, 32> {
         let l2 = self.ft.forward_from_slice(feats);
         self.l2.forward_from_i16::<ReLU, QA>(&l2)
     }
@@ -28,21 +28,20 @@ impl SubNet {
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct PolicyNetwork {
-    subnets: [[SubNet; 2]; 448],
+    subnets: [[SubNet; 2]; 128],
     hce: Layer<f32, 4, 1>,
 }
 
 impl PolicyNetwork {
     pub fn get(&self, pos: &Board, mov: &Move, feats: &[usize], threats: u64) -> f32 {
         let flip = pos.flip_val();
-        let pc = pos.get_pc(1 << mov.src()) - 1;
 
         let from_threat = usize::from(threats & (1 << mov.src()) > 0);
         let from_subnet = &self.subnets[usize::from(mov.src() ^ flip)][from_threat];
         let from_vec = from_subnet.out(feats);
 
         let good_see = usize::from(pos.see(mov, -108));
-        let to_subnet = &self.subnets[64 * pc + usize::from(mov.to() ^ flip)][good_see];
+        let to_subnet = &self.subnets[64 + usize::from(mov.to() ^ flip)][good_see];
         let to_vec = to_subnet.out(feats);
 
         let hce = self.hce.forward::<ReLU>(&Self::get_hce_feats(pos, mov)).0[0];
@@ -64,8 +63,8 @@ impl PolicyNetwork {
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct UnquantisedSubNet {
-    ft: Layer<f32, 768, 16>,
-    l2: Layer<f32, 16, 16>,
+    ft: Layer<f32, 768, 32>,
+    l2: Layer<f32, 32, 32>,
 }
 
 impl UnquantisedSubNet {
@@ -80,7 +79,7 @@ impl UnquantisedSubNet {
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct UnquantisedPolicyNetwork {
-    subnets: [[UnquantisedSubNet; 2]; 448],
+    subnets: [[UnquantisedSubNet; 2]; 128],
     hce: Layer<f32, 4, 1>,
 }
 
