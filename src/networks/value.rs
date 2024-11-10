f3ee7ee6fe10500679672f6accac969d66cf08d1
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-11-10 05:50:05 +0000

    PSQT & WDL
    bench 2762760

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -7,7 +7,7 @@ use super::{
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const ValueFileDefaultName: &str = "nn-4587c7cdf848.network";
+pub const ValueFileDefaultName: &str = "nn-faeeffb146fa.network";
 
 const QA: i16 = 512;
 const QB: i16 = 1024;
