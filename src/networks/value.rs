47c73f2f904e3015315668dc36e2dd4abe9b3aa6
jw1912 <jamie.whiting.dev@gmail.com>
2024-11-13 20:35:13 +0000

    .

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -2,7 +2,7 @@ use crate::{boxed_and_zeroed, Board};
 
 use super::{
     activation::SCReLU,
-    layer::{Layer, TransposedLayer},
+    layer::{Layer, TransposedLayer}, Accumulator,
 };
 
 // DO NOT MOVE
@@ -13,10 +13,12 @@ const QA: i16 = 512;
 const QB: i16 = 1024;
 const FACTOR: i16 = 32;
 
+const L1: usize = 4096;
+
 #[repr(C)]
 pub struct ValueNetwork {
-    l1: Layer<i16, { 768 * 4 }, 4096>,
-    l2: TransposedLayer<i16, 4096, 16>,
+    l1: Layer<i16, { 768 * 4 }, L1>,
+    l2: TransposedLayer<i16, { L1 / 2 }, 16>,
     l3: Layer<f32, 16, 128>,
     l4: Layer<f32, 128, 3>,
     pst: Layer<f32, { 768 * 4 }, 3>,
@@ -38,7 +40,28 @@ impl ValueNetwork {
 
         l2.add_multi(&feats[..count], &self.l1.weights);
 
-        let l3 = self.l2.forward_from_i16::<SCReLU, QA, QB, FACTOR>(&l2);
+        let mut act = [0; L1 / 2];
+
+        for (a, (&i, &j)) in act.iter_mut().zip(l2.0.iter().take(L1 / 2).zip(l2.0.iter().skip(L1 / 2))) {
+            let i = i32::from(i).clamp(0, i32::from(QA));
+            let j = i32::from(j).clamp(0, i32::from(QA));
+            *a = ((i * j) / i32::from(QA / FACTOR)) as i16;
+        }
+
+        let mut fwd = [0; 16];
+
+        for (f, row) in fwd.iter_mut().zip(self.l2.weights.iter()) {
+            for (&a, &w) in act.iter().zip(row.0.iter()) {
+                *f += i32::from(a) * i32::from(w);
+            }
+        }
+
+        let mut l3 = Accumulator([0.0; 16]);
+
+        for (r, (&f, &b)) in l3.0.iter_mut().zip(fwd.iter().zip(self.l2.biases.0.iter())) {
+            *r = (f as f32 / f32::from(QA * FACTOR) + f32::from(b)) / f32::from(QB);
+        }
+
         let l4 = self.l3.forward::<SCReLU>(&l3);
         let mut out = self.l4.forward::<SCReLU>(&l4);
         out.add(&pst);
@@ -62,7 +85,7 @@ impl ValueNetwork {
 #[repr(C)]
 pub struct UnquantisedValueNetwork {
     l1: Layer<f32, { 768 * 4 }, 4096>,
-    l2: Layer<f32, 4096, 16>,
+    l2: Layer<f32, 2048, 16>,
     l3: Layer<f32, 16, 128>,
     l4: Layer<f32, 128, 3>,
     pst: Layer<f32, { 768 * 4 }, 3>,
