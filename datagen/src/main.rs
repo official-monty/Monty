d069203c2598757cb37af5d6a001c5100ca328d7
jw1912 <jamie.whiting.dev@gmail.com>
2024-11-19 17:06:00 +0000

    Bench: 789803

--- datagen/src/main.rs
+++ datagen/src/main.rs
@@ -61,6 +61,8 @@ pub struct Destination {
     reusable_buffer: Vec<u8>,
     games: usize,
     limit: usize,
+    searches: usize,
+    iters: usize,
     results: [usize; 3],
 }
 
@@ -70,7 +72,7 @@ impl Destination {
             return;
         }
 
-        let result = (2.0 * game.result) as usize;
+        let result = (game.result * 2.0) as usize;
         self.results[result] += 1;
         self.games += 1;
         game.serialise_into(&mut self.writer).unwrap();
@@ -85,7 +87,7 @@ impl Destination {
         }
     }
 
-    pub fn push_policy(&mut self, game: &MontyFormat, stop: &AtomicBool) {
+    pub fn push_policy(&mut self, game: &MontyFormat, stop: &AtomicBool, searches: usize, iters: usize) {
         if stop.load(Ordering::Relaxed) {
             return;
         }
@@ -93,6 +95,9 @@ impl Destination {
         let result = (game.result * 2.0) as usize;
         self.results[result] += 1;
         self.games += 1;
+
+        self.searches += searches;
+        self.iters += iters;
 
         game.serialise_into_buffer(&mut self.reusable_buffer)
             .unwrap();
@@ -110,6 +115,8 @@ impl Destination {
     }
 
     pub fn report(&self) {
+        let average_iters = self.iters / self.searches;
+        println!("average iters {average_iters}");
         println!(
             "finished games {} losses {} draws {} wins {}",
             self.games, self.results[0], self.results[1], self.results[2],
@@ -137,6 +144,8 @@ pub fn run_datagen(
         writer: vout,
         reusable_buffer: Vec::new(),
         games: 0,
+        searches: 0,
+        iters: 0,
         limit: opts.games,
         results: [0; 3],
     };
@@ -156,7 +165,7 @@ pub fn run_datagen(
             let this_dest = dest_mutex.clone();
             s.spawn(move || {
                 let mut thread = DatagenThread::new(params.clone(), stop, this_book, this_dest);
-                thread.run(opts.nodes, opts.policy_data, policy, value);
+                thread.run(opts.policy_data, policy, value);
             });
         }
     });
