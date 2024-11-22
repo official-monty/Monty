276ce588e6027262a8612e4f6426190bdaf772d9
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-11-22 03:29:15 +0000

    fmt

--- src/mcts/helpers.rs
+++ src/mcts/helpers.rs
@@ -48,7 +48,8 @@ impl SearchHelpers {
             let mut scale = Self::base_explore_scaling(params, node);
 
             let gini = node.gini_impurity();
-            scale *= (params.gini_base() - params.gini_ln_multiplier() * (gini + 0.001).ln()).min(params.gini_min());
+            scale *= (params.gini_base() - params.gini_ln_multiplier() * (gini + 0.001).ln())
+                .min(params.gini_min());
             scale
         }
 
