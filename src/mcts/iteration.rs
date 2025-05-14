f8964eb878ccc98444a3c40a1b946f7473227b88
Shawn Xu <xu107288696@gmail.com>
2025-05-13 18:35:05 -0700

    Tweak Virtual Loss
    no functional change

--- src/mcts/iteration.rs
+++ src/mcts/iteration.rs
@@ -119,7 +119,8 @@ fn pick_action(searcher: &Searcher, ptr: NodePtr, node: &Node) -> usize {
         let threads = f64::from(child.threads());
         if threads > 0.0 {
             let visits = f64::from(child.visits());
-            let q2 = f64::from(q) * visits / (visits + threads);
+            let q2 = f64::from(q) * visits
+                / (visits + 1.0 + searcher.params.virtual_loss_weight() * (threads - 1.0));
             q = q2 as f32;
         }
 
