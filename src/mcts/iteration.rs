6f9c978f7c0e9f2b38a0874b6c0930664957f8d4
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-07-11 05:20:54 +0100

    virt dis

--- src/mcts/iteration.rs
+++ src/mcts/iteration.rs
@@ -11,6 +11,7 @@ pub fn perform_one(
     ptr: NodePtr,
     depth: &mut usize,
     thread_id: usize,
+    threads: usize,
 ) -> Option<f32> {
     *depth += 1;
 
@@ -51,7 +52,7 @@ pub fn perform_one(
         tree.fetch_children(ptr, thread_id)?;
 
         // select action to take via PUCT
-        let action = pick_action(searcher, ptr, node);
+        let action = pick_action(searcher, ptr, node, threads);
 
         let child_ptr = node.actions() + action;
 
@@ -59,7 +60,10 @@ pub fn perform_one(
 
         pos.make_move(mov);
 
-        tree[child_ptr].inc_threads();
+        let disable_vl = tree[child_ptr].visits() as usize > 1000 * threads;
+        if !disable_vl {
+            tree[child_ptr].inc_threads();
+        }
 
         // acquire lock to avoid issues with desynced setting of
         // game state between threads when threads > 1
@@ -70,11 +74,13 @@ pub fn perform_one(
         };
 
         // descend further
-        let maybe_u = perform_one(searcher, pos, child_ptr, depth, thread_id);
+        let maybe_u = perform_one(searcher, pos, child_ptr, depth, thread_id, threads);
 
         drop(lock);
 
-        tree[child_ptr].dec_threads();
+        if !disable_vl {
+            tree[child_ptr].dec_threads();
+        }
 
         let u = maybe_u?;
 
@@ -103,7 +109,7 @@ fn get_utility(searcher: &Searcher, ptr: NodePtr, pos: &ChessState) -> f32 {
     }
 }
 
-fn pick_action(searcher: &Searcher, ptr: NodePtr, node: &Node) -> usize {
+fn pick_action(searcher: &Searcher, ptr: NodePtr, node: &Node, threads: usize) -> usize {
     let is_root = ptr == searcher.tree.root_node();
 
     let cpuct = SearchHelpers::get_cpuct(searcher.params, node, is_root);
@@ -116,12 +122,14 @@ fn pick_action(searcher: &Searcher, ptr: NodePtr, node: &Node) -> usize {
         let mut q = SearchHelpers::get_action_value(child, fpu);
 
         // virtual loss
-        let threads = f64::from(child.threads());
-        if threads > 0.0 {
-            let visits = f64::from(child.visits());
-            let q2 = f64::from(q) * visits
-                / (visits + 1.0 + searcher.params.virtual_loss_weight() * (threads - 1.0));
-            q = q2 as f32;
+        if child.visits() as usize <= 1000 * threads {
+            let t = f64::from(child.threads());
+            if t > 0.0 {
+                let visits = f64::from(child.visits());
+                let q2 = f64::from(q) * visits
+                    / (visits + 1.0 + searcher.params.virtual_loss_weight() * (t - 1.0));
+                q = q2 as f32;
+            }
         }
 
         let u = expl * child.policy() / (1 + child.visits()) as f32;
