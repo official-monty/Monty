2b7edf01b5dea934178888b74793216bf3368d75
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-07-28 19:05:12 +0100

    Take 6
    Bench: 1774353

--- src/correction_history.rs
+++ src/correction_history.rs
@@ -4,7 +4,7 @@ use crate::chess::Board;
 
 /// Parameters for correction history.
 const CORRHIST_SIZE: usize = 1 << 16; // 64k entries
-const CORRHIST_WEIGHT_SCALE: i32 = 4;
+const CORRHIST_WEIGHT_SCALE: i32 = 64;
 
 pub struct CorrectionHistory {
     table: Vec<AtomicI32>,
