7eee7eb88d3023dfa7c0ab06b2b9c7c9b887535f
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-08-05 00:05:37 +0100

    Update board.rs
    6839/23441654 fails

--- src/chess/board.rs
+++ src/chess/board.rs
@@ -523,7 +523,7 @@ impl Board {
             };
 
             let our_attackers = attackers & pieces[stm] & allowed;
-            let Some((mut attacker_pc, from_bit)) =
+            let Some((attacker_pc, from_bit)) =
                 remove_least(&mut pieces, our_attackers, &mut occ)
             else {
                 break;
@@ -568,12 +568,8 @@ impl Board {
                 }
             }
 
-            if attacker_pc == Piece::PAWN
-                && ((stm == Side::WHITE && to >= 56) || (stm == Side::BLACK && to < 8))
-            {
-                score += SEE_VALS[Piece::QUEEN] - SEE_VALS[Piece::PAWN];
-                attacker_pc = Piece::QUEEN;
-            }
+            let promotion = attacker_pc == Piece::PAWN
+                && ((stm == Side::WHITE && to >= 56) || (stm == Side::BLACK && to < 8));
 
             let queens = pieces[Piece::QUEEN];
             let rooks = pieces[Piece::ROOK] | queens;
@@ -596,6 +592,9 @@ impl Board {
             }
 
             score = -score - 1 - SEE_VALS[attacker_pc];
+            if promotion {
+                score += SEE_VALS[Piece::QUEEN] - SEE_VALS[Piece::PAWN];
+            }
             stm ^= 1;
 
             pinned_w = recompute_pins(&pieces, occ, Side::WHITE, self.king_sq(Side::WHITE));
