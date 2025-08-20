447aed6a4781bd5448fb9dc7ac411e19961a85b3
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-08-20 03:52:20 +0100

    more

--- src/networks.rs
+++ src/networks.rs
@@ -10,9 +10,13 @@ pub use accumulator::Accumulator;
 // Choose the file name type based on the feature
 #[cfg(feature = "datagen")]
 pub use policy::DatagenPolicyFileName as PolicyFileDefaultName;
+#[cfg(feature = "datagen")]
+pub use value::DatagenValueFileName as ValueFileDefaultName;
 
 #[cfg(not(feature = "datagen"))]
 pub use policy::PolicyFileDefaultName;
+#[cfg(not(feature = "datagen"))]
+pub use value::ValueFileDefaultName;
 
 pub use policy::{PolicyNetwork, UnquantisedPolicyNetwork, L1 as POLICY_L1};
-pub use value::{ValueFileDefaultName, ValueNetwork};
+pub use value::{ValueNetwork};
