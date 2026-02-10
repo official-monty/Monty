c60bfe189cb4b03e6512d53d20ce09a139cbf6c3
Viren6 <94880762+Viren6@users.noreply.github.com>
2026-02-10 21:50:42 +0000

    Update value.rs

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -7,7 +7,7 @@ use super::common::{Accumulator, Layer, SCReLU, TransposedLayer};
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals, dead_code)]
-pub const ValueFileDefaultName: &str = "nn-5601bb8c241d.network";
+pub const ValueFileDefaultName: &str = "nn-58274aa39e13.network";
 #[allow(non_upper_case_globals, dead_code)]
 pub const CompressedValueName: &str = "nn-c1dd869bed55.network";
 #[allow(non_upper_case_globals, dead_code)]
